eec87ac2c6b5c6684c51e1c1cea1985ed1b815fe
Xrit <xrit412@example.com>
2023-07-03 12:03:34 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("안녕하세요");
+		System.out.println("ohlla");
+		System.out.println("Hi");
+	}
+}

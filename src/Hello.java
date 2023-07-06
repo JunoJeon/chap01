c23e97b289c0c7ae378bc3afcc280bf2d48cca38
Xrit <xrit412@gmail.com>
2023-07-06 12:01:13 +0900

    vt100 clear

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,14 @@
+public class Hello {
+	
 
-class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("안녕하세요");
-		System.out.println("ohlla");
-		System.out.println("Hi");
+
+		System.out.println("\033[2J");
+
+		System.out.println("Hello Java!");
+
+
 	}
+
+
 }

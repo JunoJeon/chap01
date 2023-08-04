779a3214ff5338ee4a441d9c58196b6b07087e70
Xrit <xrit412@gmail.com>
2023-08-04 15:59:43 +0900

    commit

---
+++ src/Comment.java
@@ -0,0 +1,28 @@
+
+/**
+ * 주석처리 방법 연구 <br>
+ * 1. 한줄 주석 기호 // <br>
+ * 2. 여러줄 주석 기호 <br>
+ * 3. 자바덕 주석 기호 <br>
+ */
+
+public class Comment {
+	
+	/*
+	 * main 메소드는 Entry Point라고 하며
+	 * 실행 클래스에는 반드시 있어야한다.
+	 * 
+	 * 한줄삭제 커맨드 : Ctrl + D
+	 * 주석 토글 키 : Ctrl + /
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hi"); // print는 New라인을 삽입하지 않는다.
+//		System.out.println("Hello Java");
+		System.out.print("Hi");
+
+	}
+
+}
